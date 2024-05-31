dbd9bae390a53b608ad36d8559b6c5fc3e54390a
Fabrício Simplício Maia <131405924+maiafs@users.noreply.github.com>
2024-05-31 12:22:37 -0300

    Documentação

--- src/pessoas/Cliente.java
+++ src/pessoas/Cliente.java
@@ -1,9 +1,26 @@
 package pessoas;
 
+/**
+ * A classe Cliente (que surge como herança da classe "Pai!" abstrata Pessoas) representa um cliente com 
+ * atributos adicionais como email, CNH e data de nascimento.
+ */
+
 public class Cliente extends Pessoa {
     private String email;
     private String cnh;
     private String dataNascimento;
+
+/**
+ * Construtor para criar um novo cliente.
+ *
+ * @param nome o nome do cliente
+ * @param cpf o CPF do cliente
+ * @param dataNascimento a data de nascimento do cliente
+ * @param endereco o endereço do cliente
+ * @param telefone o telefone do cliente
+ * @param email o email do cliente
+ * @param cnh a CNH do cliente
+ */
 
     public Cliente(String nome, String cpf, String dataNascimento, String endereco, String telefone, String email, String cnh) {
         super(cpf.replace(".", "").replace("-", ""), nome, cpf, endereco, telefone);
@@ -11,8 +28,25 @@ public class Cliente extends Pessoa {
         this.cnh = cnh;
         this.dataNascimento = dataNascimento;
     }
-
-    // Getters e setters
+ /**
+ * Obtém o email do cliente.
+ * @return o email do cliente
+ * 
+ * Define o email do cliente.
+ * @param email o email do cliente
+ * 
+ * Obtém a CNH do cliente.
+ * @return a CNH do cliente
+ * 
+ * Define a CNH do cliente.
+ * @param cnh a CNH do cliente
+ * 
+ * Obtém a data de nascimento do cliente.
+ * @return a data de nascimento do cliente
+ * 
+ * Define a data de nascimento do cliente.
+ * @param dataNascimento a data de nascimento do cliente 
+ */
     public String getEmail() { return this.email; }
     public String getCnh() { return this.cnh; }
     public String getDataNascimento() { return this.dataNascimento; }
@@ -21,7 +55,10 @@ public class Cliente extends Pessoa {
     public void setCnh(String cnh) { this.cnh = cnh; }
     public void setDataNascimento (String dataNascimento) {this.dataNascimento = dataNascimento; }
 
-
+/**
+* Obtem as informações do cliente formatadas.
+* @return as informações do cliente
+*/
     @Override
     public String getInfo() {
         return String.format("Informações de cliente: \n\tId: %s\n\tNome: %s\n\tCPF: %s\n\tCNH: %s\n\tEndereço: %s\n\tTelefone: %s\n", getId(), getNome(), getCpf(), getCnh(), getEndereco(), getTelefone());
--- src/pessoas/Funcionario.java
+++ src/pessoas/Funcionario.java
@@ -1,24 +1,55 @@
 package pessoas;
+/**
+ * A classe Funcionario (que herda da classe abstrata Pessoa) representa um funcionário 
+ * com atributos adicionais como cargo e salário.
+ */
 
 public class Funcionario extends Pessoa {
     private String cargo;
     private double salario;
-
+/**
+ * Construtor para criar um novo Funcionário.
+ *
+ * @param id o identificador único do funcionário
+ * @param nome o nome do funcionário
+ * @param cpf o CPF do funcionário
+ * @param endereco o endereço do funcionário
+ * @param telefone o telefone do funcionário
+ * @param cargo o cargo do funcionário
+ * @param salario o salário do funcionário
+ */
     public Funcionario(String id, String nome, String cpf, String endereco, String telefone, String cargo, double salario) {
         super(id, nome, cpf, endereco, telefone);
         this.cargo = cargo;
         this.salario = salario;
     }
 
-    
-    // Getters e setters
+/**
+ * Cada método getter e setter é documentado com uma breve descrição do que ele faz, e,
+ * quando aplicável, dos parâmetros e valores de retorno.
+ * 
+ * Obtém o cargo do funcionário.
+ * @return o cargo do funcionário
+ * 
+ * Define o cargo do funcionário.
+ * @param cargo o cargo do funcionário
+ * 
+ * Obtém o salário do funcionário.
+ * @return o salário do funcionário
+ * 
+ * Define o salário do funcionário.
+ * @param salario o salário do funcionário
+ */
     public String getCargo() { return this.cargo; }
     public double getSalario() { return this.salario; }
 
     public void setCargo(String cargo) { this.cargo = cargo; }
     public void setSalario(double salario) { this.salario = salario; }
 
-
+/**
+ * Obtém as informações do funcionário formatadas.
+ * @return as informações do funcionário
+ */
     @Override
     public String getInfo() {
         return String.format("Informações de funcionário: \n\tId: %s\n\tNome: %s\n\tCargo: %s", getId(), getNome(), getCargo());
